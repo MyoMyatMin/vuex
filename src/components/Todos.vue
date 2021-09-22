d46392d32b51fd2e4da6ce2fc6e2a3ec13d5361e
myomyatmin <myomyatmin315@gmail.com>
2021-09-22 12:53:51 +0630

    first commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <h3 class="text-primary text-center">All todos</h3>
+    <div class="container">
+        <div class="row">
+            <AddTodos></AddTodos>
+        </div>
+        <div class="row">
+            <div class="col-md-4 my-4" v-for="todo in myTodos" :key="todo.id">
+                <b-card bg-variant="primary" text-variant="white" class="text-center">
+                    <b-card-text class="d-flex justify-content-between"><span>{{todo.title}}</span>
+                    <span @click="deleteTodo(todo.id)"><b-icon icon="trash-fill" variant="danger"></b-icon></span></b-card-text>
+                  </b-card>
+            </div>
+        </div>
+    </div>
+    </div>    
+</template>
+
+<script>
+import AddTodos from './AddTodos'
+import {mapActions, mapGetters} from 'vuex';
+export default {
+  components: { AddTodos },
+    computed:mapGetters(["myTodos"]),
+    methods:mapActions(['getTodos',"deleteTodo"]),
+    mounted(){
+        this.getTodos();
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/store/modules/Todo.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+export default {
+    state:{
+        todos : [
+           
+        ]
+    },
+    getters:{
+        myTodos(state){
+            return state.todos
+        }
+    },
+    mutations:{
+        setTodos(state,todos){
+            state.todos = todos;
+        },
+        setTodo(state,newTodo){
+            state.todos.unshift(newTodo)
+        },
+        removeTodo(state,removeId){
+            state.todos = state.todos.filter(todo=>{
+                return todo.id != removeId
+            })
+        }
+    },
+    actions:{
+       async getTodos({commit}){
+             let res = await axios.get("https://jsonplaceholder.typicode.com/todos")
+             console.log(res);
+             let todos = res.data;
+             commit('setTodos',todos)
+        },
+        async addTodo(context,newTodo){
+          let res = await axios.post("https://jsonplaceholder.typicode.com/todos",newTodo) 
+          context.commit('setTodo',res.data); 
+        },
+        async deleteTodo(context,removeId){
+            await axios.delete(`https://jsonplaceholder.typicode.com/todos/${removeId}`)
+            context.commit('removeTodo',removeId);
+        }
+    }
+}
